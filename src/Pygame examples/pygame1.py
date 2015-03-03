4e0a12dcf10b1a4baaf0b313c317896727be8a8e
agentwaffles <jpriest@gmail.com>
2015-03-03 18:49:38 -0500

    added collision detection

--- src/Pygame examples/pygame1.py
+++ src/Pygame examples/pygame1.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import time
 
 pygame.init()
 
@@ -24,6 +25,26 @@ def car(x,y):
     gameDisplay.blit(carImg, (x,y))   #0,0 being top left
 
 
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+
+def message_display(text):
+    largeText = pygame.font.SysFont('Arial', 115)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+
+    pygame.display.update()
+    time.sleep(2)
+    game_loop()
+
+
+def crash():
+    message_display('You Crashed')
+
+
 
 def game_loop():
     x = (display_height * 0.45)
@@ -36,7 +57,9 @@ def game_loop():
     while not gameExit:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                gameExit = True
+                pygame.quit()
+                quit()
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     x_change = -5
@@ -50,13 +73,10 @@ def game_loop():
         car(x,y)
 
         if x > display_width - car_width or x < 0:
-            gameExit = True
+            crash()
 
         pygame.display.update()
         clock.tick(60)
-
-
-
 
 
 game_loop()
