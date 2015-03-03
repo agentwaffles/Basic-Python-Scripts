c1aa0e6a92164aaae618d9cf48bc49c7f5efe333
agentwaffles <jpriest@gmail.com>
2015-03-03 07:59:56 -0500

    Minor updates to game

--- src/Pygame examples/pygame1.py
+++ src/Pygame examples/pygame1.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 
 pygame.init()
 
@@ -10,6 +11,7 @@ white = (255,255,255)
 red = (255,0,0)
 green = (0,255,0)
 blue = (0,0,255)
+car_width = 73
 
 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
@@ -21,23 +23,44 @@ carImg =pygame.image.load('racecar.png')
 def car(x,y):
     gameDisplay.blit(carImg, (x,y))   #0,0 being top left
 
-x = (display_height * 0.45)
-y = (display_height * 0.48)
+
+
+def game_loop():
+    x = (display_height * 0.45)
+    y = (display_height * 0.8)
+
+    x_change = 0
+
+    gameExit = False
+
+    while not gameExit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 5
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
+        x += x_change
+        gameDisplay.fill(white)
+        car(x,y)
+
+        if x > display_width - car_width or x < 0:
+            gameExit = True
+
+        pygame.display.update()
+        clock.tick(60)
 
 
 
-crashed = False
 
-while not crashed:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
 
-    gameDisplay.fill(white)
-    car(x,y)
-    pygame.display.update()
-    clock.tick(60)
-
+game_loop()
 pygame.quit()
-quit()
+sys.exit()
+#quit()
 
